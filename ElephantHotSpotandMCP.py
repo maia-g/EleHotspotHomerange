e6a117b20b2b4bbfbaf3774e5c2b963d72d606d0
maia-g <maia.griffith@duke.edu>
2024-11-18 12:12:40 -0500

    Creating scripts to begin project

---
+++ ElephantHotSpotandMCP.py
@@ -0,0 +1,12 @@
+##---------------------------------------------------------------------
+## ElephantHotSpotandMCP.py
+##
+## Description: Read in elephant collar tracking data and create a
+##    hot spot visualization using hexagons and home range using Minimum Convex Polygons
+##
+## Usage: Analyze elephant movement spatially
+##
+## Created: Fall 2024
+## Author: maia.griffith@duke.edu (for ENV859)
+##---------------------------------------------------------------------
+
---
+++ Preparation_SingleDataset.py
@@ -0,0 +1,11 @@
+##---------------------------------------------------------------------
+## Preparation_SingleDataset.py
+##
+## Description: Select data to be prepared for hot spot analysis
+##
+## Usage: Transform Excel to Table (.dbf), make points from the XY coordinates, and project.
+##
+## Created: Fall 2024
+## Author: maia.griffith@duke.edu (for ENV859)
+##---------------------------------------------------------------------
+
