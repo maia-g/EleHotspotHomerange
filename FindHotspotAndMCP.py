1f7ac2d2541c76539fa3719f69833b28db2d3e2d
maia-g <maia.griffith@duke.edu>
2024-11-21 11:39:40 -0500

    stuck on summarize within in hotspots

--- FindHotspotAndMCP.py
+++ FindHotspotAndMCP.py
@@ -17,3 +17,86 @@
 import os
 import sys
 import arcpy
+
+# Allow user input for scratch workspace, output workspace, and dataset.
+##### UNCOMMENT BELOW WHEN READY TO GO IN ARCPRO###############
+# scratch = arcpy.GetParameterAsText(0) #Sets scratch workspace where most interim model outputs will go
+# output = arcpy.GetParameterAsText(1) #Sets output workspace where important outputs from model will go
+scratch = "V:\\859FinalProject_mhg29\\BassConnections_OrphanHotspots\\Scratch"
+output = "V:\\859FinalProject_mhg29\\BassConnections_OrphanHotspots\\BassConnections_OrphanHotspots.gdb"
+
+# Set environment settings
+arcpy.env.overwriteOutput = True #Make sure to exit interaction window in VS Code before re-running code
+arcpy.EnvManager(scratchWorkspace= scratch, workspace= output)
+arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("WGS_1984_UTM_Zone_35S")
+
+##### UNCOMMENT BELOW WHEN READY TO GO IN ARCPRO###############
+# dataset = arcpy.GetParameterAsText(2) #User input in ArcPro
+dataset = "V:\\859FinalProject_mhg29\\BassConnections_OrphanHotspots\\Outputs\\Batoka_move_SubsetDry_Sept_Nov_Points.shp" 
+name = os.path.splitext(os.path.basename(dataset))[0].split("Nov")[0] + "Nov" 
+    #Removes file extension type then ensures the file name ends with Nov regaurdless of suffix
+#print(name)
+
+# Create bounding box/extent with user input
+##### UNCOMMENT BELOW WHEN READY TO GO IN ARCPRO###############
+# topLat = arcpy.GetParameterAsText(3) #User input in ArcPro
+# bottomLat = arcpy.GetParameterAsText(4) #User input in ArcPro
+# leftLon = arcpy.GetParameterAsText(5) #User input in ArcPro
+# rightLon = arcpy.GetParameterAsText(6) #User input in ArcPro
+topLat = -15.55
+bottomLat = -26.00
+leftLon = 25.75
+rightLon = 26.15
+
+##------Remove Outliers and Create Tessellation------
+
+# Remove outliers by only keeping data within bounding box using: Select
+selectedPoints = f"{scratch}\\{name}_Select.shp"
+bounds = f""" Latitude < {topLat} AND
+              Latitude > {bottomLat} AND 
+              Longitude > {leftLon} AND 
+              Longitude < {rightLon} 
+          """
+arcpy.analysis.Select(in_features= dataset, 
+                      out_feature_class= selectedPoints, 
+                      where_clause= bounds)
+
+# Create hexagons using: Generate Tessellation
+hexGrid = f"{scratch}\\HexGrid_1km.shp"
+tessExtent = arcpy.Describe(selectedPoints).extent #Extent of selected points
+projection = arcpy.SpatialReference(32735) #Ensure WGS UTM Zone 35S
+arcpy.management.GenerateTessellation(Output_Feature_Class= hexGrid,
+                                      Extent= tessExtent,
+                                      Shape_Type="HEXAGON",
+                                      Size="866025.4 SquareMeters", #Makes side height 1km
+                                      H3_Resolution=7,
+                                      Spatial_Reference= projection #Makes sure project is correct
+                                      )
+
+##------Find Hotspots------
+
+# Calculate total count per hexagon using: Summarize Within
+dataHotspot = f"{output}\\{name}_Hotspots.shp"
+arcpy.analysis.SummarizeWithin(hexGrid, #input polygons
+                               selectedPoints, #input features
+                               dataHotspot, #output file 
+                               "", "", "", #skip some options
+                               "SQUAREKILOMETERS")
+
+# Check the spatial reference of hexGrid and selectedPoints to make sure they match
+hexGrid_desc = arcpy.Describe(hexGrid)
+selectedPoints_desc = arcpy.Describe(selectedPoints)
+
+# Print out spatial references to ensure they match
+print(f"hexGrid Spatial Reference: {hexGrid_desc.spatialReference.name}")
+print(f"selectedPoints Spatial Reference: {selectedPoints_desc.spatialReference.name}")
+
+# Process: Minimum Bounding Geometry (Minimum Bounding Geometry) (management)
+Merged_OrphEle_MCP = "R:\\Wildlife\\GIS\\Batoka_Maia\\BassConnections_OrphanHotspots\\BassConnections_OrphanHotspots.gdb\\Merged_OrphEle_MCP"
+with arcpy.EnvManager(extent="24.5232215310188 -16.5104707575485 27.0014261251794 -14.9065151970758 GEOGCS[\"GCS_WGS_1984\",DATUM[\"D_WGS_1984\",SPHEROID[\"WGS_1984\",6378137.0,298.257223563]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]]"):
+    arcpy.management.MinimumBoundingGeometry(in_features=Merged_OrphEle_NoOutliers_shp, out_feature_class=Merged_OrphEle_MCP, geometry_type="CONVEX_HULL")
+
+
+
+
+
--- Preparation_SingleDataset.py
+++ Preparation_SingleDataset.py
@@ -24,7 +24,7 @@ import arcpy
 # output = arcpy.GetParameterAsText(2) #Sets output workspace where important outputs from model will go
 dataset = "V:\\859FinalProject_mhg29\\BassConnections_OrphanHotspots\\Data\\Orphan_Ele_Movement_Data\\Batoka_move_SubsetDry_Sept_Nov.xlsx" 
 scratch = "V:\\859FinalProject_mhg29\\BassConnections_OrphanHotspots\\Scratch"
-output = "V:\\859FinalProject_mhg29\\BassConnections_OrphanHotspots\\Outputs"
+output = "V:\\859FinalProject_mhg29\\BassConnections_OrphanHotspots\\BassConnections_OrphanHotspots.gdb"
 name = os.path.splitext(os.path.basename(dataset))[0] #Save only the base file name
 
 # Set environment settings
@@ -32,20 +32,26 @@ arcpy.env.overwriteOutput = True #Make sure to exit interaction window in VS Cod
 arcpy.EnvManager(scratchWorkspace= scratch, workspace= output)
 arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("WGS_1984_UTM_Zone_35S")
 
+##------Check Path------
+
 # Create a describe object with the dataset
 dsc_dataset = arcpy.da.Describe(dataset) #Creating the "Describe" object
 ###print(dsc.keys()) #Uncomment to see keys
 
 # Indicate catalogPath directory
 arcpy.AddMessage(f"The dataset's catalog path is: {dsc_dataset['catalogPath']}")
+arcpy.AddMessage(f"The outputs path is: {output}")
+arcpy.AddMessage(f"The scratch folder path is: {scratch}")
 ###print(f"The dataset's catalog path is: {dsc['catalogPath']}")
+
+##------Begin Processing------
 
 # Convert selected data set using: Excel To Table
 Output_Table = f"{scratch}\\{name}_Table.dbf"
 arcpy.conversion.ExcelToTable(Input_Excel_File= dataset, Output_Table= Output_Table)
 
-# Convert new table into points using: XY Table To Point
-dataset_points = f"{scratch}\\{name}_Points.shp"
+# Convert new table into points and save to Outputs folder using: XY Table To Point
+dataset_points = f"{output}\\{name}_Points.shp"
 arcpy.management.XYTableToPoint(in_table= Output_Table, 
                                 out_feature_class= dataset_points, 
                                 x_field="Longitude", y_field="Latitude")
@@ -53,4 +59,4 @@ arcpy.management.XYTableToPoint(in_table= Output_Table,
     # Check the spatial reference of the new shapefile
 dsc_points = arcpy.da.Describe(dataset_points)
 arcpy.AddMessage(f"The new points shapefile Coordinate System is: {dsc_points['spatialReference'].name}")
-print(f"The new points shapefile Coordinate System is: {dsc_points['spatialReference'].name}")
+#print(f"The new points shapefile Coordinate System is: {dsc_points['spatialReference'].name}")
